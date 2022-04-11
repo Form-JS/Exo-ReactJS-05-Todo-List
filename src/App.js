f52d7fa251f48a2ceabda11df2ae44d9adcabace
Aurélien <aurelien.strimelle@bstorm.be>
2022-04-11 16:03:20 +0200

    Exo 05

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import MainHeader from './containers/main-header/main-header';
+import MainFooter from './containers/main-footer/main-footer';
+import TodoApp from './containers/todo-app/todo-app';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MainHeader />
+      <main>
+        <TodoApp />
+      </main>
+      <MainFooter />
     </div>
   );
 }
